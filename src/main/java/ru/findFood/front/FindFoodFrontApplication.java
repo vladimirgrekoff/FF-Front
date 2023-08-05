3c225f92db23fea483744f01e352f0249cf7bd19
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-08-05 14:35:28 +0700

    PA10T20 create Main class

---
+++ src/main/java/ru/findFood/front/FindFoodFrontApplication.java
@@ -0,0 +1,12 @@
+package ru.findFood.front;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindFoodFrontApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FindFoodFrontApplication.class, args);
+    }
+}
